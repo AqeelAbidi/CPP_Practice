77e8831a88638a40a7de027c42330b86ce52feb4
Muhammad Aqeel <130394025+AqeelAbidi@users.noreply.github.com>
2023-04-10 23:09:11 +0500

    Create Student_Details.cpp

---
+++ Student_Details.cpp
@@ -0,0 +1,26 @@
+                //-----------:0:AQEEL_ABiDi:0:-----------//
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout<<"\t\t\t\t\tStudent Details"<<endl;
+	cout<<"plz Enter the rollno for student detail"<<endl;
+	int a;
+	cin>>a;
+	if(a==1)
+	{
+		cout<<"Student Name : Asif\nfather name : Abbas\nObtain marks : 450";
+	}
+    else if(a==2)
+	{
+		cout<<"Student Name : Abid\nfather name : Hashim\nObtain marks : 410";
+	}
+    else if(a==3)
+	{
+		cout<<"Student Name : Kazim\nfather name : Ali\nObtain marks : 400";	
+	}
+	else
+	{
+		cout<<"This student is not in this list";
+	}
+}
